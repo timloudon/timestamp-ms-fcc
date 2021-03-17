a2ee0e73f7d668b8e6f63cc0309a83b9221332fc
timloudon1 <timloudon@gmail.com>
2021-03-17 15:35:13 +0000

    Created basic structure for API

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 let express = require('express');
 let app = express();
 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+var listener = app.listen(process.env.PORT, () => 
+  console.log(`App is listening on port ${listener.address().port}`));
