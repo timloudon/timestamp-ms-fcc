6916c5f1fdf9d5b8df3e4fa0a7e79c78306b5aba
timloudon1 <timloudon@gmail.com>
2021-03-18 10:15:26 +0000

    Readability improvements

--- server.js
+++ server.js
@@ -2,45 +2,29 @@ let express = require('express');
 let app = express();
 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-app.use(express.static('public'));
+app.use( express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
-app.get('/api/timestamp/', (req, res, next) => {
-  const now = new Date();
-  res.json({
-    unix: now.getTime(),
-    utc: now.toUTCString()
-  })
-})
+app.get('/api/timestamp/', (req, res) => res.json({ unix: new Date().getTime(), utc: new Date().toUTCString() }))
 
 app.get('/api/timestamp/:date', (req, res) => {
-  const userInput = req.params.date;
-  if (/^\+?(0|[1-9]\d*)$/.test(userInput)) {
-    const userInputAsInt = parseInt(userInput); 
-
-    res.json({
-      unix: userInputAsInt,
-      utc: new Date(userInputAsInt).toUTCString()
-    })
+  const userDateStr = req.params.date;
+  // check the userDateStr is digits
+  if ( /^\+?(0|[1-9]\d*)$/.test( userDateStr ) ) {
+    const userDateInt = parseInt( userDateStr );
+    res.json({ unix: userDateInt, utc: new Date(userDateInt).toUTCString() })
   } else {
-    const userDateObject = new Date(userInput);
-    console.log('userDateObject: ', userDateObject, `is a ${typeof userDateObject}`)
-    console.log('userDateObject as UTC: ', userDateObject.toUTCString());
+    const userDateObject = new Date(userDateStr);
     if (userDateObject.toString() === "Invalid Date") {
-      res.json({error: "Invalid Date"});
+      res.json({ error: "Invalid Date" });
     } else {
-    res.json({
-      unix: userDateObject.valueOf(),
-      utc: userDateObject.toUTCString()
-    })
+      res.json({ unix: userDateObject.valueOf(), utc: userDateObject.toUTCString() })
     }
   }
 })
 
-var listener = app.listen(process.env.PORT, () => 
+var listener = app.listen(process.env.PORT, () =>
   console.log(`App is listening on port ${listener.address().port}`));
